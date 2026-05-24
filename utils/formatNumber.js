00fb7b58e515c9ac6c14fe001a3a69682f577163
Eternallygr8 <112281650+Eternallygr8@users.noreply.github.com>
2026-05-24 23:34:35 +0530

    Update formatNumber.js

--- utils/formatNumber.js
+++ utils/formatNumber.js
@@ -1,1 +1,10 @@
+function formatNumber(num) {
+  if (num >= 1e12) return (num / 1e12).toFixed(2) + 'T';
+  if (num >= 1e9) return (num / 1e9).toFixed(2) + 'B';
+  if (num >= 1e6) return (num / 1e6).toFixed(2) + 'M';
+  if (num >= 1e3) return (num / 1e3).toFixed(2) + 'K';
 
+  return num.toString();
+}
+
+module.exports = formatNumber;
